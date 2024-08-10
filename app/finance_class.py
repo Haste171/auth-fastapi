ae48fa7bea213dde4a3cc478b4f677ff39617ea5
jthom2 <134821369+jthom2@users.noreply.github.com>
2024-08-10 07:26:56 -0500

    Update finance_class.py

--- app/finance_class.py
+++ app/finance_class.py
@@ -63,27 +63,3 @@ class FinanceData:
         finally:
             cur.close()
             conn.close()
-
-    @classmethod
-    def plot_transactions(df):
-        df.set_index('date', inplace=True)
-
-        income_df = df[df['category'] == 'Income'].resample('D').sum().reindex(df.index, fill_value=0)
-        expense_df = df[df['category'] == 'Expense'].resample('D').sum().reindex(df.index, fill_value=0)
-
-        # Create the plot
-        plt.figure(figsize=(10, 5))
-        plt.plot(income_df.index, income_df['amount'], label='Income', color='green')
-        plt.plot(expense_df.index, expense_df['amount'], label='Expense', color='red')
-        plt.xlabel('Date')
-        plt.ylabel('Amount')
-        plt.title('Income and Expenses vs Time')
-        plt.legend()
-        plt.grid(True)
-
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png')
-        plt.close()
-        buf.seek(0)
-
-        return buf
